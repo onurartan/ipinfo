b10d1c1ef90349f0f2c586c09f878b88a8368499
ban0 <artanomer13@gmail.com>
2024-06-17 10:06:15 +0300

    Update Project

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Request
+from geoip2.database import Reader
+
+app = FastAPI()
+
+# GeoLite2 veritabanı dosyasının yolu
+GEOIP_DATABASE_PATH = './db/GeoLite2-City.mmdb'
+
+
+# GeoLite2 veritabanı okuyucusunu oluşturun
+reader = Reader(GEOIP_DATABASE_PATH)
+
+@app.get("/getip")
+def get_client_ip(request: Request):
+    client_ip = request.client.host
+    
+    return client_ip
+
+@app.get("/ipinfo")
+def get_client_ip(request: Request):
+    client_ip = request.client.host
+
+    try:
+        # IP adresinden coğrafi bilgi alın
+        response = reader.city(client_ip)
+        country = response.country.name
+        city = response.city.name
+        latitude = response.location.latitude
+        longitude = response.location.longitude
+
+        return {
+            "client_ip": client_ip,
+            "country": country,
+            "city": city,
+            "latitude": latitude,
+            "longitude": longitude
+        }
+    except Exception as e:
+        return {"error": str(e)}
