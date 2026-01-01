8ad9ca7cecae176b001f8d9fa0374ca30d8e048c
onurartan <artanomer13@gmail.com>
2026-01-01 17:06:50 +0300

    fix: updated geoip2 error fixed

--- app/services.py
+++ app/services.py
@@ -6,54 +6,40 @@ from .schema import Response, ErrorResponse
 
 GEOIP_DATABASE_PATH = "./db/GeoLite2-City.mmdb"
 
-reader = Reader(GEOIP_DATABASE_PATH)
 
-
-# if you use 3.10 python version -> Response | ErrorResponse
 def get_ip_info(ip_address: str, user_agent: str) -> Union[Response, ErrorResponse]:
     try:
-        # Get geographic info from IP address
-        response = reader.city(ip_address)
-        country = response.country.name
-        city = response.city.name
-        latitude = response.location.latitude
-        longitude = response.location.longitude
-        continent = response.continent.names["en"]
-        time_zone = response.location.time_zone
-        postal = response.postal.code
-        isp = response.traits.isp
-        organization = response.traits.organization
-        reader.close()
+        with Reader(GEOIP_DATABASE_PATH) as reader:
+            response = reader.city(ip_address)
 
-        data = {
-            "success": True,
-            "status": 200,
-            "client_ip": ip_address,
-            "country": country,
-            "city": city,
-            "latitude": latitude,
-            "longitude": longitude,
-            "time_zone": time_zone,
-            "continent": continent,
-            "posta_code": postal,
-            "isp": isp,
-            "organization": organization,
-            "user_agent": {
-                "user_agent_str": user_agent,
-                **parse_user_agent(user_agent),
-            },
-        }
+            data = {
+                "success": True,
+                "status": 200,
+                "client_ip": ip_address,
+                "country": response.country.name,
+                "city": response.city.name,
+                "latitude": response.location.latitude,
+                "longitude": response.location.longitude,
+                "time_zone": response.location.time_zone,
+                "continent": response.continent.names.get("en"),
+                "posta_code": response.postal.code,
+                "isp": response.traits.isp,
+                "organization": response.traits.organization,
+                "user_agent": {
+                    "user_agent_str": user_agent,
+                    **parse_user_agent(user_agent),
+                },
+            }
 
         return Response(**data)
-    except Exception as e:
 
-        error_data = {
-            "success": False,
-            "status": 400,
-            "error": str(e),
-            "user_agent": {
+    except Exception as e:
+        return ErrorResponse(
+            success=False,
+            status=400,
+            error=str(e),
+            user_agent={
                 "user_agent_str": user_agent,
                 **parse_user_agent(user_agent),
             },
-        }
-        return ErrorResponse(**error_data)
+        )
